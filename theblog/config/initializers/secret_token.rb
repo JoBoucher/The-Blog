b71ddec9be8b2191bcc44cd959300fb819fe6f27
John <zedfelos@gmail.com>
2011-10-20 19:20:44 -0600

    The Blog is ready for updating, captain

---
+++ theblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theblog::Application.config.secret_token = 'd978f08f2fa8e8cc40147ce501ebd9fe240000a1d24f8465c3481efb1e1b72e3c68050da35bac4b475362041667610170952e4f82c9f8b98b32068c4744df631'
---
+++ theblog/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Theblog::Application.config.session_store :cookie_store, key: '_theblog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Theblog::Application.config.session_store :active_record_store
